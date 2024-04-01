e1e4a5633086eba24e43fe701d805e915419e590
aekkakti <music.tomsk1@gmail.com>
2024-04-01 11:54:37 +0700

    Полиморфизм

--- index.php
+++ index.php
@@ -1,70 +1,26 @@
 <?php
-interface CalculateSquare
+
+class A
 {
-    public function calculateSquare(): float;
+    public function method1()
+    {
+        return $this->method2();
+    }
+
+    protected function method2()
+    {
+        return 'A';
+    }
 }
-class Rectangle
+
+class B extends A
 {
-
-    private $x;
-    private $y;
-
-    public function __construct(float $x, float $y) {
-        $this->x = $x;
-        $this->y = $y;
-    }
-
-    public function calculateSquare(): float
+    protected function method2()
     {
-        return $this->x * $this->y;
+        return 'B';
     }
 }
 
-class Square implements CalculateSquare
-{
-    private $x;
+$b = new B();
 
-    public function __construct(float $x) {
-        $this->x = $x;
-    }
-
-    public function calculateSquare(): float
-    {
-        return $this->x ** 2;
-    }
-}
-
-class Circle implements CalculateSquare
-{
-    const PI = 3.1416;
-    private $r;
-
-    public function __construct(float $r)
-    {
-        $this->r = $r;
-    }
-
-    public function calculateSquare():float
-    {
-        return self::PI * ($this->r ** 2);
-    }
-}
-
-$objects = [
-    new Square(5),
-    new Rectangle(2, 4),
-    new Circle(5)
-];
-
-foreach ($objects as $object) {
-    if ($object instanceof CalculateSquare) {
-        $name = get_class($object);
-        echo 'Объект реализует интерфейс CalculateSquare. Площадь: ' . $object->calculateSquare()  . '. Это результат высчитывания площади для ' . $name;
-        echo '<br>';
-    }
-    else {
-        $name = get_class($object);
-        echo 'Объекте класса  $name не реализует интерфейс CalculateSquare ';
-        echo '<br>';
-    }
-}
+echo $b->method1();
