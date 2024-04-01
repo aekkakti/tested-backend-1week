5878e5935eaa61205f4669732e0c1fe81b89be8e
aekkakti <music.tomsk1@gmail.com>
2024-04-01 10:57:48 +0700

    Инкапсуляция

--- files.php
+++
@@ -1,4 +0,0 @@
-<?php
-$data = 'abc' . PHP_EOL . 'def' . PHP_EOL;
-file_put_contents(__DIR__ . 'file.txt', $data, FILE_APPEND);
-echo $data;
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$login = $_POST['login'] ?? '';
-$password = $_POST['password'] ?? '';
-setcookie('login', $login, 0);
-setcookie('password', $password, 0);
-header('Location: index.php');
