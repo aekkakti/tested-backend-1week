c2bd8f5a51d06e3a5a50f5885da7e1be1ca4c4aa
aekkakti <music.tomsk1@gmail.com>
2024-04-01 14:22:32 +0700

    Объектно-ориентированный подход

--- index.php
+++ index.php
@@ -1,22 +1,67 @@
 <?php
 
-class Human
+class User
 {
-    private static $count = 0;
+    private $name;
 
-
-    public function __construct()
-    {
-        self::$count++;
+    public function __construct(string $name) {
+        $this->name = $name;
     }
 
-    public static function getCount()
+    public function getName(): string
     {
-        return self::$count;
+        return $this->name;
     }
 }
 
-$human1 = new Human();
-$human2 = new Human();
-$human3 = new Human();
-echo 'Людей уже ' . Human::getCount();
+class Admin extends User
+{
+
+}
+
+class Cat
+{
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
+
+class Article
+{
+    private $title;
+    private $text;
+    private $author;
+
+    public function __construct(string $title, string $text, User $author)
+    {
+        $this->title = $title;
+        $this->text = $text;
+        $this->author = $author;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+}
+
+$author = new Admin('Пётр');
+$article = new Article('Заголовок', 'Текст', $author);
