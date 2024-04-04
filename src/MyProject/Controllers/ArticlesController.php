3444ae299c9d41dfcdb8d42cabfaffe5d4e0ffae
aekkakti <music.tomsk1@gmail.com>
2024-04-04 10:34:56 +0700

    Active Record

---
+++ src/MyProject/Controllers/ArticlesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MyProject\Controllers;
+
+use MyProject\Models\Articles\Article;
+use MyProject\Models\Users\User;
+use MyProject\View\View;
+
+class ArticlesController
+{
+    /** @var View */
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View(__DIR__ . '/../../../templates');
+    }
+
+    public function view(int $articleId)
+    {
+        $article = Article::getById($articleId);
+
+        if ($article === null) {
+            $this->view->renderHtml('errors/404.php', [], 404);
+            return;
+        }
+
+        $articleAuthor = User::getById($article->getAuthorId());
+
+        $this->view->renderHtml('articles/view.php', [
+            'article' => $article,
+            'author' => $articleAuthor
+        ]);
+    }
+}
