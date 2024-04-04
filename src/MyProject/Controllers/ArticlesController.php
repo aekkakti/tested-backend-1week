74a05b5541d2c5704d3453f917fdfd93c871bece
aekkakti <music.tomsk1@gmail.com>
2024-04-04 11:06:17 +0700

    PHP Reflection API

--- src/MyProject/Controllers/ArticlesController.php
+++ src/MyProject/Controllers/ArticlesController.php
@@ -20,6 +20,15 @@ class ArticlesController
     {
         $article = Article::getById($articleId);
 
+        $reflector = new \ReflectionObject($article);
+        $properties = $reflector->getProperties();
+        $propertiesNames = [];
+        foreach ($properties as $property) {
+            $propertiesNames[] = $property->getName();
+        }
+        var_dump($propertiesNames);
+        return;
+
         if ($article === null) {
             $this->view->renderHtml('errors/404.php', [], 404);
             return;
